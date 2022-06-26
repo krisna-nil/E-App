88f1f4049bb807b6febab050c4f47e32fe784856
krisna-nil <nilesh11374@gmail.com>
2022-06-26 13:56:53 +0530

    shopping list added

--- E-app/src/app/shopping-list/shopping-list.component.ts
+++ E-app/src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {  Ingredient } from './ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients:Ingredient[]=[
+    new Ingredient('Apple',5),
+    new Ingredient('potato',10),
+    new Ingredient('banana',5),
+    new Ingredient('carrot',5)
 
+  ];
   constructor() { }
 
   ngOnInit(): void {
