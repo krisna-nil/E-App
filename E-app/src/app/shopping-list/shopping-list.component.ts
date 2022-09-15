c529fc0293629734d23e6c185c56832928f0d82b
krisna-nil <nilesh11374@gmail.com>
2022-09-15 18:15:58 +0530

    basic structure of app created by section 3 course project the basics

--- E-app/src/app/shopping-list/shopping-list.component.ts
+++ E-app/src/app/shopping-list/shopping-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {  Ingredient } from './ingredient.model';
+import {  Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
